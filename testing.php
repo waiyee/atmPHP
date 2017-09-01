3761fc446693b24d2a164573e396815a478b7b1f
waiyee <wynntsui@gmail.com>
2017-09-01 12:47:37 +0800

    testing to see buying selling and net profit

--- testing.php
+++ testing.php
@@ -3,13 +3,23 @@ include('app.php');
 
 $OB = $dbclient->coins->OwnOrderBook;
 
-$orders = $OB->find(array('Status'=>'sold'));
+$orders = $OB->find([]);
 $net_profit = 0;
+$buying_btc = 0;
+$selling_btc = 0;
 foreach ($orders as $order)
 {
-
-    $net_profit = $net_profit + ($order->SellOrder->Total - $order->BuyOrder->Total);
+    if ($order->Status == 'selling' )
+        $selling_btc += $order->BuyOrder->Total;
+    if ($order->Status == 'buying')
+        $buying_btc += $order->BuyOrder->Total;
+    if ($order->Status == 'sold') {
+        $selling_btc += $order->SellOrder->Total;
+        $net_profit = $net_profit + ($order->SellOrder->Total - $order->BuyOrder->Total);
+    }
 }
 
-var_dump($net_profit);
+echo 'Buying hold ' . $buying_btc . ' BTC<br/>';
+echo 'Selling hold ' . $selling_btc . ' BTC <br/>';
+echo 'Net Profit ' . $net_profit . ' BTC<br/>';
 
