415c88ff65a2f79cc097d0d93bdc79de19789384
da Vinci Frank <vincidafrank@gmail.com>
2017-09-03 01:20:02 +0800

    Introduce Osward for order status checking

---
+++ Osward.php
@@ -0,0 +1,32 @@
+<?php
+include('app.php');
+/**
+Hello I'm Osward, the orders monitor.
+ **/
+
+$OOB = $dbclient->coins->OwnOrderBook;
+
+$buying_orders = $OOB->find(
+    array('Status' => 'buying')
+);
+foreach ($buying_orders as $buying_order){
+    $status = $bittrex->getOrder($buying_order->BuyOrder->uuid);
+    if (!empty($status->Closed)){
+        boughtOrder($buying_order->_id,'bought',$dbclient);
+    }
+}
+
+$selling_orders = $OOB->find(
+    array('Status' => 'selling')
+);
+foreach ($selling_orders as $selling_order){
+    $status = $bittrex->getOrder($selling_order->SellOrder->uuid);
+    if (!empty($status->Closed)){
+        soldOrder($selling_order->_id,'sold',$dbclient);
+    }
+}
+
+
+
+
+?>
